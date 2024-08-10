8096e383349cffaea44bcedfc33d48f71bd32ddf
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2024-08-10 23:51:57 +0200

    Add environment tag
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var (
 			Name: "aws_cost",
 			Help: "AWS cost in USD",
 		},
-		[]string{"service", "region"},
+		[]string{"service", "region", "environment"},
 	)
 )
 
@@ -52,6 +52,10 @@ func fetchCostData() {
 				Type: aws.String("DIMENSION"),
 				Key:  aws.String("REGION"),
 			},
+			{
+				Type: aws.String("TAG"),
+				Key:  aws.String("Environment"),
+			},
 		},
 	}
 
@@ -64,13 +68,14 @@ func fetchCostData() {
 	for _, group := range result.ResultsByTime[0].Groups {
 		service := *group.Keys[0]
 		region := *group.Keys[1]
+		environment := *group.Keys[2]
 		amount := *group.Metrics["UnblendedCost"].Amount
 		cost, err := strconv.ParseFloat(amount, 64)
 		if err != nil {
 			log.Printf("Failed to parse cost for service %s: %v", service, err)
 			continue
 		}
-		costGauge.WithLabelValues(service, region).Set(cost)
+		costGauge.WithLabelValues(service, region, environment).Set(cost)
 	}
 }
 
